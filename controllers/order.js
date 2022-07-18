01e40672ff2b6fc7ec75eb6e1c00973daf5f9739
Mahmoud Fahiem <xmahmoud92@gmail.com>
2022-07-18 03:42:05 +0200

    style: Use module.exports to export order controller

--- controllers/order.js
+++ controllers/order.js
@@ -1,10 +1,10 @@
 const orderModel = require("../models/Order");
 
-export const getAllOrders = (req, res) => {
+const getAllOrders = (req, res) => {
   res.send('Get All Customers Data !!');
 }
 
-export const createOrder = async (req, res) => {
+const createOrder = async (req, res) => {
   try {
     const savedOrder = new orderModel(req.body);
     await savedOrder.save();
@@ -14,7 +14,7 @@ export const createOrder = async (req, res) => {
   }
 }
 
-export const getOrder = async (req, res) => {
+const getOrder = async (req, res) => {
   try {
     const users = await orderModel.find({ _id: req.params.id }); //to be populated if wanted
     res.status(200).send(users);
@@ -23,7 +23,7 @@ export const getOrder = async (req, res) => {
   }
 }
 
-export const updateOrder = async (req, res) => {
+const updateOrder = async (req, res) => {
   try {
     const result = await orderModel.updateOne({ _id: req.params.id }, req.body);
     res.status(200).send(result);
@@ -32,11 +32,19 @@ export const updateOrder = async (req, res) => {
   }
 }
 
-export const deleteOrder = async (req, res) => {
+const deleteOrder = async (req, res) => {
   try {
     const result = await orderModel.deleteOne({ _id: req.params.id });
     res.status(200).send(result);
   } catch (err) {
     res.send();
   }
+}
+
+module.exports = {
+  getAllOrders,
+  createOrder,
+  getOrder,
+  updateOrder,
+  deleteOrder
 }
