b1c74a041b16fe8635a9ff160389f3a60c7a884e
User <mnasser480@gmail.com>
2022-07-22 18:55:17 +0200

    Feat/User: add user auth

--- server.js
+++ server.js
@@ -14,16 +14,18 @@ app.options('*', cors());
 app.use(express.json());
 dotenv.config();
 
+
+mongoose.connect(process.env.MONGO_URL)
+    .then( ()=> console.log('db connected successfully'))
+    .catch( err =>console.log(err));
+
+
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/order', orderRoute);
 app.use('/api/v1/products', productRoute);
 app.use('/api/v1/wishlist', wishlistRoute);
 
 
-
-mongoose.connect(process.env.MONGO_URL)
-    .then( ()=> console.log('db connected successfully'))
-    .catch( err =>console.log(err));
 
 app.listen(process.env.PORT||3000,()=>{
     console.log('Backend server is running!');
