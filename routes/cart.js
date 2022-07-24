f05b91789fbbe8384c579ca0ec9a1dfcfa8bb6e6
User <mnasser480@gmail.com>
2022-07-25 01:00:35 +0200

    feat:cart add cart routes

---
+++ routes/cart.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const CartModel = require('../models/Cart');
+const Router = express.Router();
+const cartController = require('../controllers/cart');
+
+// add Cart Item
+Router.post('/',auth,cartController.addCartItem);
+// Update Cart Items
+Router.patch('/',auth,cartController.updateCart);
+// get all customer cart items
+Router.get('/',auth,cartController.getAllCartItems);
+
+module.exports = Router;
