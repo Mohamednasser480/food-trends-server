6c75fd972a9fb6e9490ecea5e497d0143b22c501
User <mnasser480@gmail.com>
2022-07-25 22:19:45 +0200

    refactor/product: add product controller file

--- routes/product.js
+++ routes/product.js
@@ -1,63 +1,12 @@
 const express = require('express');
-const productModel = require('../models/product')
 const auth = require('../middleware/auth');
+const productController = require('../controllers/product');
+const Router = express.Router();
 
-const product = express.Router();
+Router.get('/',auth,productController.getAllProducts);
+Router.get('/:id',auth,productController.getProduct);
+Router.post('/',auth,productController.addProduct);
+Router.delete('/:id',auth,productController.deleteProduct);
+Router.patch('/:id',auth,productController.updateProduct);
 
-product.get('/',auth,async (req,res)=>{
-    try{
-        const products = await productModel.find({vendor:req.user._id});
-         res.status(200).send(products);
-        //res.status(200).send(products);
-    }catch (err){
-        res.status(400).send(err);
-    }
-});
-
-
-product.get('/:id',auth,async (req,res)=>{
-    try{
-        const product = await productModel.findOne({_id:req.params.id, vendor:req.user._id});
-        if(!product) return res.status(404).send();
-        res.status(200).send(product);
-    }catch (err){
-        res.status(400).send(err);
-    }
-});
-
-
-product.post('/',auth,async (req,res)=>{
-    try {
-        const savedProduct = new productModel({...req.body,vendor:req.user._id});
-        await savedProduct.save();
-        res.status(201).send(savedProduct);
-    }catch (err){
-        res.status(400).send(err);
-    }
-});
-
-product.delete('/:id',auth,async (req,res)=>{
-    try{
-        const product = await productModel.findOneAndDelete({_id:req.params.id, vendor:req.user._id});
-        if(!product) return res.status(404).send();
-        res.send(product);
-    }catch (e){
-        res.status(400).send(e);
-    }
-});
-product.patch('/:id',auth,async (req,res)=>{
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'summary','description','images','category','price','inStock','discount'];
-    const isValidUpdate = updates.every( update => allowedUpdates.includes(update));
-    if(!isValidUpdate) return res.status(400).send({error:'Invalid update!'});
-    try{
-        const updatedProduct = await productModel.findOneAndUpdate({_id:req.params.id,vendor:req.user._id},req.body,{new:true,runValidators:true});
-        if(!updatedProduct) return res.status(404).send();
-        updates.forEach( update=> updatedProduct[update] = req.body[update]);
-        await updatedProduct.save();
-        res.send(updatedProduct);
-    }catch (e){
-        res.status(400).send(e);
-    }
-})
-module.exports = product;
+module.exports = Router;
