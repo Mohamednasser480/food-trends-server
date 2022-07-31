90d232fc2fb456971102382739c5b9f7001b067d
User <mnasser480@gmail.com>
2022-07-31 18:13:46 +0200

    feat/product:get similar products sorted by the product rate

--- controllers/product.js
+++ controllers/product.js
@@ -50,10 +50,25 @@ const updateProduct = async (req,res)=>{
         res.status(400).send(e);
     }
 }
+
+const getMostSimilar = async (req,res)=>{
+    try{
+        const product = await productModel.findById(req.params.id);
+        if(!product) res.status(404).send();
+        const similarProducts = await productModel.find({category:product.category});
+        if(!similarProducts) res.status(404).send();
+        // points.sort(function(a, b){return b - a});
+        similarProducts.sort((a,b)=> (b.rate/b.numberOfReviews) - (a.rate/a.numberOfReviews));
+        res.send(similarProducts.slice(0,4));
+    }catch (e){
+        res.status(400).send(e);
+    }
+}
 module.exports = {
     getAllProducts,
     getProduct,
     addProduct,
     deleteProduct,
-    updateProduct
+    updateProduct,
+    getMostSimilar
 }
