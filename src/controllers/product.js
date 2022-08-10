389460d65cd9be4e91906ff3e137e5bd71058de5
User <mnasser480@gmail.com>
2022-08-10 13:41:50 +0200

    feature/product: add total number of products in the response

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -11,21 +11,23 @@ const getAllProducts = async (req,res)=>{
             skip:req.query.skip,
             sort:sort
         }
+
         const filterObj = {};
         const maxPrice = req.query.max_price;
         const minPrice = req.query.min_price;
         if(maxPrice && minPrice) filterObj.price = { $lte:maxPrice,$gte:minPrice };
-        if(req.query.category){
+        if(req.query.category)
             filterObj.category = {"$regex": req.query.category,'$options':'im'};
-            console.log(filterObj)
-        }
+
         if(req.query.search){
             req.query.search = req.query.search.toLowerCase();
             filterObj.productName = {"$regex": req.query.search,'$options':'i'};
         }
+        const count = await productModel.find(filterObj,null).count();
         const products = await productModel.find(filterObj,null,options);
         if(!products) return res.status(404).send();
-        res.send(products);
+
+        res.send({data:products,count});
     }catch (err){
         console.log(err);
         res.status(400).send(err);
