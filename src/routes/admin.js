ec5ab9bfc7c86541d596e7af76bf7a0c7a81c955
User <mnasser480@gmail.com>
2022-08-17 14:34:44 +0200

    feature/admin: add delete and approve product routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,9 @@ const adminAuth = require('../middleware/admin');
 const Router = express.Router();
 
 Router.get('/users',[auth,adminAuth],adminController.getUsers);
-Router.put('/users/:id',[auth,adminAuth],adminController.changeStatus);
+Router.put('/users/:id',[auth,adminAuth],adminController.changeUserStatus);
 Router.delete('/users',[auth,adminAuth],adminController.deleteUser);
 Router.get('/products',[auth,adminAuth],adminController.getProducts);
+Router.put('/products',[auth,adminAuth],adminController.changeProductStatus);
+
 module.exports = Router;
